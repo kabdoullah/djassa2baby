83b10529d0266d478545b75d5fa11b6d252f60b7
Kemogoha Abdoulaye Coulibaly <abdoullahcoulibaly2@gmail.com>
2024-04-06 21:10:35 +0000

    add url to ALLOWED_HOSTS

--- djassa2baby/djassa2baby/settings.py
+++ djassa2baby/djassa2baby/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','djassa2baby.pythonanywhere.com']
 CORS_ALLOWED_ORIGINS = [
     "https://djassa2baby.pythonanywhere.com",
-    "http://localhost:3000",
+    "http://localhost:4000",
 ]
 
 # Application definition
