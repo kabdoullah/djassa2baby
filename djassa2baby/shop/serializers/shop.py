4427c80fdf3bfd8a4c784bd5ce7da58d0ab15cce
kouamecorneille <smcorneille@gmail.com>
2024-08-27 19:20:31 +0000

    fixe create shop with owner and ctagorie

--- djassa2baby/shop/serializers/shop.py
+++ djassa2baby/shop/serializers/shop.py
@@ -5,7 +5,8 @@ from shop.models.subscription import Subscription
 from users.models import User
 from shop.models.shop import Shop, ShopReview
 import ast
-
+from django.db import transaction, IntegrityError
+import json
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
@@ -44,36 +45,49 @@ class ShopOwnerSerializer(serializers.Serializer):
     whatsapp_link = serializers.URLField(required=False, allow_null=True)
     instagram_link = serializers.URLField(required=False, allow_null=True)
     twitter_link = serializers.URLField(required=False, allow_null=True)
-    categories = serializers.ListField(child=serializers.UUIDField(), required=False, allow_null=True)  # New field
+    categories = serializers.CharField( required=False, allow_null=True)  # New field
 
     def create(self, validated_data):
-        # Extract password and handle user creation separately
-        password = validated_data.pop('password')
-        categories = validated_data.pop('categories', [])
-        # Create the user associated with this shop
-        vendor_role = Role.objects.get(label='vendeur')  # Assuming role retrieval is correct
-        subscription = Subscription.objects.get(name='Gratuit')
+        try:
+            # Démarrer une transaction atomique
+            with transaction.atomic():
+                # Extract password and handle user creation separately
+                password = validated_data.pop('password')
+                categories = validated_data.pop('categories', None)
+                # Create the user associated with this shop
+                vendor_role = Role.objects.get(label='vendeur')  # Assuming role retrieval is correct
+                subscription = Subscription.objects.get(name='Gratuit')
 
-        user = User.objects.create_user(
-            phone_number=validated_data['phone_number_1'],
-            password=password,
-            email=validated_data.get('email'),
-            role=vendor_role
-        )
-        
-        # Assign the user to the validated data and create the shop
-        validated_data['user'] = user
-        validated_data['subscription'] = subscription
-        shop = Shop.objects.create(**validated_data)
+                user = User.objects.create_user(
+                    phone_number=validated_data['phone_number_1'],
+                    password=password,
+                    email=validated_data.get('email'),
+                    role=vendor_role
+                )
+                
+                # Assign the user to the validated data and create the shop
+                validated_data['user'] = user
+                validated_data['subscription'] = subscription
+                shop = Shop.objects.create(**validated_data)
+                # Convert the categories string back to a list of UUIDs
+                liste_uuids = ast.literal_eval(categories)
+                # Handle categories
+                for categorie_id in liste_uuids:
+                    category = Category.objects.get(id=categorie_id)
+                    ShopCategorie.objects.create(
+                        shop=shop,
+                        category=category
+                    )
 
-        liste_uuids = ast.literal_eval(categories)
-        # Handle categories
-        for categorie_id in liste_uuids:
-            category = Category.objects.get(id=categorie_id)
-            ShopCategorie.objects.create(
-                shop=shop,
-                category=category
-            )
+                return shop
 
-        return shop
+        except IntegrityError as e:
+            # Gérer l'erreur d'intégrité, par exemple les duplications de clé ou les erreurs de contrainte
+            print(f"IntegrityError occurred: {str(e)}")
+            raise serializers.ValidationError("Une erreur s'est produite lors de la création de la boutique.")
+
+        except Exception as e:
+            # Gérer toute autre erreur
+            print(f"An error occurred: {str(e)}")
+            raise serializers.ValidationError("Une erreur s'est produite lors de la création de la boutique.")
 
