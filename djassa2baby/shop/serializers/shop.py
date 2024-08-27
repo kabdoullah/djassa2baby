bc565e7baf5930f7d242b60d8d8f14a4d058fd62
kouamecorneille <smcorneille@gmail.com>
2024-08-27 13:17:04 +0000

    add ShopOwnerSerializer for create shop with owner

--- djassa2baby/shop/serializers/shop.py
+++ djassa2baby/shop/serializers/shop.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from core.models.role import Role
+from shop.models.product import Category, ShopCategorie
 from shop.models.subscription import Subscription
 from users.models import User
 from shop.models.shop import Shop, ShopReview
@@ -28,39 +29,50 @@ class ShopReviewSerializer(serializers.ModelSerializer):
 
 class ShopOwnerSerializer(serializers.Serializer):
     """
-        Serializer for creating a shop with an owner.
+    Serializer for creating a shop with an owner.
     """
     name = serializers.CharField(max_length=255)
-    logo = serializers.ImageField(required=False)
+    logo = serializers.ImageField(required=True)
     email = serializers.EmailField()
     phone_number_1 = serializers.CharField(max_length=20)
     password = serializers.CharField(write_only=True)  # Write-only to avoid exposing it
     phone_number_2 = serializers.CharField(max_length=20, required=False, allow_null=True)
     description = serializers.CharField(required=False, allow_blank=True)
     location = serializers.CharField(max_length=255)
-    subscription = serializers.UUIDField(required=False,allow_null=True)
+    subscription = serializers.UUIDField(required=False, allow_null=True)
     facebook_link = serializers.URLField(required=False, allow_null=True)
     whatsapp_link = serializers.URLField(required=False, allow_null=True)
     instagram_link = serializers.URLField(required=False, allow_null=True)
     twitter_link = serializers.URLField(required=False, allow_null=True)
+    categories = serializers.ListField(child=serializers.CharField(), required=False, allow_null=True)  # New field
 
     def create(self, validated_data):
         # Extract password and handle user creation separately
         password = validated_data.pop('password')
+        categories = validated_data.pop('categories', [])
         # Create the user associated with this shop
-        vendror_role = Role.objects.get(label='vendeur')  # Assuming role retrieval is correct
+        vendor_role = Role.objects.get(label='vendeur')  # Assuming role retrieval is correct
         subscription = Subscription.objects.get(name='Gratuit')
 
         user = User.objects.create_user(
             phone_number=validated_data['phone_number_1'],
             password=password,
             email=validated_data.get('email'),
-            role=vendror_role
+            role=vendor_role
         )
         
         # Assign the user to the validated data and create the shop
         validated_data['user'] = user
         validated_data['subscription'] = subscription
-        return Shop.objects.create(**validated_data)
+        shop = Shop.objects.create(**validated_data)
 
+        # Handle categories
+        for categorie_id in categories:
+            category = Category.objects.get(id=categorie_id)
+            ShopCategorie.objects.create(
+                shop=shop,
+                category=category
+            )
+
+        return shop
 
--- djassa2baby/shop/views/shop.py
+++ djassa2baby/shop/views/shop.py
@@ -88,6 +88,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
+
 class CreateShopWithOwnerAPIView(APIView):
     """
     API view to create a new shop along with the shop owner.
